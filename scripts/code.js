4ea7248e09a20c6428036c4772be4c32e2639bed
Potito Gradone <pgradone@gmail.com>
2020-06-01 19:41:14 +0200

    Add files via upload

---
+++ scripts/code.js
@@ -0,0 +1,42 @@
+// Inspired by https://blog.bitsrc.io/data-structures-in-javascript-part-1-8231c9a4bc8b
+
+myQueue = () => {
+  const queue = [];
+  return {
+    add(x) {
+      queue.unshift(x);
+    },
+    remove() {
+      return queue.pop();
+    },
+    next() {
+      return queue[queue.length - 1];
+    },
+    length() {
+      return queue.length;
+    },
+    empty() {
+      return queue.length === 0;
+    },
+    list() {
+      return queue;
+    },
+  };
+};
+
+const mainQueue = myQueue();
+const enterMainQueueButton = document.querySelector('.enter');
+const exitMainQueueButton = document.querySelector('.exit');
+const clientList = document.querySelector('.list');
+let clientNumber = 0;
+
+enterMainQueueButton.addEventListener('click', () => {
+  clientNumber += 1;
+  mainQueue.add('client' + clientN);
+  clientList.textContent = mainQueue.list;
+});
+
+exitMainQueueButton.addEventListener('click', () => {
+  mainQueue.remove();
+  clientList.textContent = mainQueue.list;
+});
